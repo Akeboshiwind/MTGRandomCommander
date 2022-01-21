081fe1232531d80531b8686e8bed3403b697b557
Oliver Marshall <olivershawmarshall@gmail.com>
2022-01-21 18:55:10 +0000

    Add a raw_search function

--- main.py
+++ main.py
@@ -192,13 +192,22 @@ class ColourConverter:
 id_to_colour = ColourConverter()
 
 
-def search(query):
-    "Performs a search to scryfall with some basic ratelimiting"
+def raw_search(query):
     time.sleep(0.1)
     logging.debug("Searching for: `" + query + "`")
     try:
-        return scrython.cards.Search(q=query, unique="cards", dir="asc").data()
+        return scrython.cards.Search(q=query, unique="cards", dir="asc")
     except ScryfallError:
+        return None
+
+
+def search(query):
+    "Performs a search to scryfall with some basic ratelimiting"
+    resp = raw_search(query)
+
+    if resp:
+        return resp.data()
+    else:
         return []
 
 
